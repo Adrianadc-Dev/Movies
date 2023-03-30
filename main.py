623dc1fbb9a3ebfc133b7a127cabe810e7c48158
Adriana Diaz <addiaz802@gmail.com>
2023-03-30 10:09:50 -0500

    add http methods

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 from fastapi.responses import HTMLResponse
 
 app = FastAPI()
@@ -42,4 +42,40 @@ def get_movie(id: int):
             return item
         else:
              return []
+        
+@app.get ('/movies/', tags = ['movies'])
+def get_movies_by_category(category:str, year:int):
+    return [ item for item in movies if item['category'] == category]
 
+
+@app.post('/movies', tags = ['movies']) #agregar una nueva pelicula
+def create_movie(id: int = Body(), title: str = Body(), overview : str = Body(), year: int = Body(), rating: float = Body(), category: str = Body()):
+    movies.append({
+        'id' : id,
+        'title': title,
+        'overview' : overview,
+        'year': year,
+        'rating': rating,
+        'category' : category
+
+    })
+    return movies
+
+@app.put('/movies/{id}', tags = ['movies']) #para modificar una pelicula
+def update_movie(id: int, title: str = Body(), overview : str = Body(), year: int = Body(), rating: float = Body(), category: str = Body()):
+    for item in movies:
+        if item['id'] == id:
+            item ['title'] = title
+            item ['overview'] = overview
+            item ['year'] = year
+            item ['rating'] = rating
+            item ['category'] = category
+            return movies
+
+@app.delete('/movies/{id}', tags = ['movies']) # para eliminar
+def delete_movie(id: int):
+    for item in movies:
+        if item['id'] == id:
+            movies.remove(item)
+            return movies
+        
